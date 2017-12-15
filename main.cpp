4f89489b42137dacfc3e29c724a37f52a224c30c
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-12-15 22:38:02 +0300

    Implement threads

--- main.cpp
+++ main.cpp
@@ -1,7 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <thread>
 #include "Factorizer.h"
+
+bool ex = false;
+bool paused = false;
+
+std::mutex lockcalc;
+std::mutex lockinput;
+std::condition_variable cv;
+
+void factorize (std::ifstream& ifs, std::ofstream& ofs, char* output) {
+
+    while (ifs.good()) {
+
+        if (!ofs.is_open()) {
+            ofs.open(output, std::fstream::app);
+        }
+
+        std::unique_lock<std::mutex> locker(lockcalc);
+
+        uint64_t number;
+        ifs >> number;
+
+        Factorizer f(number);
+
+        std::string factors = f.toString();
+
+        ofs << number << ": " << factors << std::endl;
+
+        if (ex) {
+            break;
+        }
+
+        while (paused) {
+            cv.wait(locker);
+            ofs.close();
+        }
+
+    }
+
+    ifs.close();
+    ofs.close();
+
+}
+
+void input() {
+
+
+    std::unique_lock<std::mutex> locker(lockinput);
+
+    std::string in;
+    while (!ex) {
+
+        std::cin >> in;
+
+        if (in == "exit") {
+            ex = true;
+        }
+
+        if (in == "pause") {
+            paused = true;
+        }
+
+        if (in == "resume") {
+            paused = false;
+            cv.notify_one();
+        }
+
+    }
+
+}
 
 int main(int argc, char* argv[]) {
 
@@ -13,29 +83,19 @@ int main(int argc, char* argv[]) {
     std::ifstream ifs;
     std::ofstream ofs;
 
-    ifs.open(argv[1], std::ifstream::in);
-    ofs.open(argv[2], std::ifstream::out);
+    ifs.open(argv[1], std::fstream::in);
+    ofs.open(argv[2], std::fstream::out);
 
     if (!ifs.good()) {
         std::cout << "Can't open input file" << std::endl;
         exit(1);
     }
 
-    while (ifs.good()) {
+    std::thread t1(factorize, std::ref(ifs), std::ref(ofs), argv[2]);
+    std::thread t2(input);
 
-        uint64_t number;
-        ifs >> number;
-
-        Factorizer f(number);
-
-        std::string factors = f.toString();
-
-        ofs << number << ": " << factors << std::endl;
-
-    }
-
-    ifs.close();
-    ofs.close();
+    t1.join();
+    t2.join();
 
     return 0;
 }
